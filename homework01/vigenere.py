e242aa11f5fb7d571d7106599628e7db507d38a8
Ulianaburd <uliana.burd.2004@gmail.com>
2022-11-30 21:14:25 +0300

    done

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,4 +1,47 @@
-from homework01.caesar import decrypt_caesar, encrypt_caesar
+def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+    """
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    alf_low = alf.lower()
+    ciphertext = ""
+    for i in plaintext:
+        if i in alf:
+            m = alf.index(i)
+            n = (m + shift) % 26
+            ciphertext += alf[n]
+        elif i in alf_low:
+            m = alf_low.index(i)
+            n = (m + shift) % 26
+            ciphertext += alf_low[n]
+        else:
+            ciphertext += i
+
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+    """
+    Decrypts a ciphertext using a Caesar cipher.
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("Sbwkrq3.6")
+    'Python3.6'
+    >>> decrypt_caesar("")
+    ''
+    """
+    return encrypt_caesar(ciphertext, -shift)
 
 
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
